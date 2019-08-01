6de91513bc91bde74cc0d33724bf7ad9c7f5a4ab
CHEN-YI HUANG <chenyee@cs.nctu.edu.tw>
2019-08-02 02:02:15 +0800

    :penguin: Add vue style test

--- demo/App.vue
+++ demo/App.vue
@@ -2,15 +2,20 @@
   <div id="app">
     <h1>Demo</h1>
     <ul>
-      <li v-for="(date, index) in order_demo" v-bind:key="index">{{ date }}</li>
+      <li
+        v-for="(date, index) in order_demo"
+        :key="index"
+      >
+        {{ date }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-import moment from "moment";
+import moment from 'moment';
 
-import DateRepetition from "../src/repetition";
+import DateRepetition from '../src/repetition';
 
 export default {
   data() {
@@ -19,31 +24,31 @@ export default {
   computed: {
     after_demo() {
       const currentDate = new Date();
-      const after_option = {
+      const afterOption = {
         dateStart: currentDate,
         weekDayList: [0, 1, 2],
         durationAmount: 1,
-        durationUnit: "w"
+        durationUnit: 'w',
       };
 
-      const list = new DateRepetition(after_option).afterOccurances(3);
+      const list = new DateRepetition(afterOption).afterOccurances(3);
 
       return list;
     },
     until_demo() {
       const currentDate = new Date();
 
-      const until_option = {
+      const untilOption = {
         dateStart: currentDate,
         weekDayList: [moment(currentDate).day()],
         durationAmount: 2,
-        durationUnit: "w"
+        durationUnit: 'w',
       };
 
-      const list = new DateRepetition(until_option).untilFinishDate(
+      const list = new DateRepetition(untilOption).untilFinishDate(
         moment(currentDate)
-          .add(1, "M")
-          .toDate()
+          .add(1, 'M')
+          .toDate(),
       );
 
       return list;
@@ -51,18 +56,17 @@ export default {
     order_demo() {
       const currentDate = new Date(2019, 7, 28);
 
-      const order_after_option = {
+      const orderAfterOption = {
         dateStart: currentDate,
         durationAmount: 1,
-        durationUnit: "M",
-        ordinalWeek: true
+        durationUnit: 'M',
+        ordinalWeek: true,
       };
 
-      const list = new DateRepetition(order_after_option).afterOccurances(4);
+      const list = new DateRepetition(orderAfterOption).afterOccurances(4);
 
       return list;
-    }
-  }
+    },
+  },
 };
 </script>
-
