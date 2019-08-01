6079062b95c4308b5db2110a6a43b958307c17f5
CHEN-YI HUANG <chenyee@cs.nctu.edu.tw>
2019-08-01 15:52:01 +0800

    :penguin: Add isEqualDiff for test

--- test/after.test.js
+++ test/after.test.js
@@ -1,34 +1,34 @@
 /* eslint-disable no-restricted-syntax */
-/* global test expect */
+/* global test expect week */
 
 import moment from 'moment';
 import faker from 'faker';
 
 import DateRepetition from '../index';
+import week from '../week';
 
-test('Validate the day repeition', () => {
-  const currentDate = faker.date.recent();
-  const day = moment(currentDate).day();
-  const afterOption = {
-    dateStart: currentDate,
-    weekDayList: [day], // , (day + 1) % 7, (day + 2) % 7],
-    durationAmount: 1,
-    durationUnit: 'w',
-  };
-  let lastDate = currentDate;
-  const len = 10;
-  const list = new DateRepetition(afterOption).afterOccurances(len);
-  expect(list.length).toBe(len * afterOption.weekDayList.length);
-  for (const [i, d] of list.entries()) {
-    const momentDay = moment(d);
-    if (i === 0) {
-      expect(momentDay.diff(lastDate, 'week')).toBe(0);
-    } else {
-      expect(momentDay.diff(lastDate, 'week')).toBe(1);
+function formatDate(d) {
+  return moment(d).format('YYYY/MM/DD');
+}
+
+function isEqualDiff(durationAmount, durationUnit) {
+  return (status, cur, i, array) => {
+    if (i < 2) {
+      return true;
     }
-    lastDate = momentDay;
-  }
-});
+
+    const first = array[i - 2];
+    const second = array[i - 1];
+    const third = array[i];
+
+    return status && (second.diff(first, durationUnit) === third.diff(second, durationUnit))
+      && (third.diff(second, durationUnit) === durationAmount);
+  };
+}
+
+// test('Validate the day repeition', () => {
+
+// });
 
 test('Validate the day repetition of monthly ordinal week', () => {
   const currentDate = faker.date.recent(); // The fourth Sunday in July
@@ -39,11 +39,31 @@ test('Validate the day repetition of monthly ordinal week', () => {
     durationUnit: 'M',
     ordinalWeek: true,
   };
+  // const dateStartWeekOrder = week.weekOrder(orderAfterOption.dateStart);
   const len = 4;
   const list = new DateRepetition(orderAfterOption).afterOccurances(len);
   expect(list.length).toBe(len);
+  expect(formatDate(list[0])).toBe(formatDate(orderAfterOption.dateStart));
   for (const d of list) {
     const momentDay = moment(d);
     expect(momentDay.day()).toBe(day);
+    // expect(week.weekOrder(momentDay)).toBe(dateStartWeekOrder);
   }
+});
+
+test('Validate the day repetition in days', () => {
+  const currentDate = faker.date.recent(); // The fourth Sunday in July
+  const afterOption = {
+    dateStart: currentDate,
+    durationAmount: 1,
+    durationUnit: 'd',
+  };
+
+  const len = 10;
+  const list = new DateRepetition(afterOption).afterOccurances(len);
+  expect(list.length).toBe(len);
+  expect(formatDate(list[0])).toBe(formatDate(afterOption.dateStart));
+  expect(list.reduce(
+    isEqualDiff(afterOption.durationAmount, afterOption.durationUnit),
+  )).toBe(true);
 });
